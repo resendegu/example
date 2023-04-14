0495f6c0c3de2102d8182db5ef731acc9a99eea5
Gustavo Resende <gustavo@resende.app>
2023-04-14 11:22:39 -0300

    Version 1.5

--- agent/Windows/example.ps1
+++
@@ -1,9 +0,0 @@
-
-$xml = '<EXAMPLE>'
-$xml += '<COLUMN_1> 42 </COLUMN_1>'
-$xml += '<COLUMN_2> TEXT DATA EXAMPLE </COLUMN_2>'
-$xml += '</EXAMPLE>'
-
-
-[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
-[Console]::WriteLine($xml)
--- ms_example/ms_example.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo "Nothing to see here"
-?>
-
